78e4dd926b869af81e8755ef1bf8d15282e4dfbb
Pattable <77807709+Pattable@users.noreply.github.com>
2026-03-01 23:16:54 -0700

    Unified Tappables
    Objects can now respawn as well

--- FoxyForest/Assets/Scripts/Tappable.cs
+++ FoxyForest/Assets/Scripts/Tappable.cs
@@ -1,15 +1,103 @@
+using System.Collections;
 using UnityEngine;
+using TouchScript.Gestures;
+
+public enum objectID
+{
+    Cherry,
+    Mushroom,
+    Gem,
+    Skunk,
+    Raccoon
+}
 
 public class Tappable : MonoBehaviour
 {
-    [SerializeField] public int scoreValue = 0; // Score value for tapping this object
+    [SerializeField] public objectID objectID;
 
-    public void OnTapped()
+    private PressGesture pressGesture;
+
+    void Awake()
+    {
+       pressGesture = GetComponent<PressGesture>();
+    }
+
+    void OnEnable()
+    {
+        pressGesture.Pressed += PressedHandler;
+    }
+
+    void OnDisable()
+    {
+        pressGesture.Pressed -= PressedHandler;
+    }
+
+    private void PressedHandler(object sender, System.EventArgs e)
     {
         if (GameManager.Instance != null && GameManager.Instance.isGameRunning)
         {
-            GameManager.Instance.AddScore(scoreValue);
-            gameObject.SetActive(false); // Deactivate the object after tapping
+            StartCoroutine(waitRespawn());
+        }
+    }
+
+    private IEnumerator waitRespawn()
+    {
+        SpriteRenderer sprite = GetComponent<SpriteRenderer>();
+
+        if (!sprite.enabled) yield break;
+        sprite.enabled = false;
+
+        GameManager.Instance.AddScore(getScoreFromID(objectID));
+        yield return new WaitForSeconds(getRespawnTimeFromID(objectID));
+
+        sprite.enabled = true;
+    }
+
+    private int getScoreFromID(objectID id)
+    {
+        switch (id)
+        {
+            case objectID.Cherry:
+                return 10;
+
+            case objectID.Mushroom:
+                return 420;
+
+            case objectID.Gem:
+                return 69;
+            
+            case objectID.Skunk:
+                return -50;
+
+            case objectID.Raccoon:
+                return -100;
+
+            default:
+                return 0;
+        }
+    }
+
+    private float getRespawnTimeFromID(objectID id)
+    {
+        switch (id)
+        {
+            case objectID.Cherry:
+                return Random.Range(1f, 3f);
+
+            case objectID.Mushroom:
+                return Random.Range(2f, 4f);
+
+            case objectID.Gem:
+                return Random.Range(3f, 5f);
+            
+            case objectID.Skunk:
+                return Random.Range(1f, 3f);
+
+            case objectID.Raccoon:
+                return Random.Range(2f, 4f);
+
+            default:
+                return 0f;
         }
     }
 }
