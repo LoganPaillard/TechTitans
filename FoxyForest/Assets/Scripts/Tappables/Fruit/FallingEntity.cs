563bf231e015b748fa57f5692889b2b44bc73635
LoganPaillard <paillardl@mymacewan.ca>
2026-03-30 21:13:14 -0600

    Added a shake
    Falling items now have a little shake when they are done growing to indicate they are going to fall.

--- FoxyForest/Assets/Scripts/Tappables/Fruit/FallingEntity.cs
+++ FoxyForest/Assets/Scripts/Tappables/Fruit/FallingEntity.cs
@@ -8,6 +8,11 @@ public class FallingEntity : Fruit
     public float fallPositionLower = -4f;
     private float fallPosition;
     public float fallDuration = 1f;
+
+    [Header("Shake Settings")]
+    public float shakeDuration = 0.5f;
+    public float shakeMagnitude = 0.1f;
+    public float shakeSpeed = 20f;
     protected Vector3 _spawnPosition;
     protected bool _isFalling = false;
 
@@ -34,7 +39,24 @@ public class FallingEntity : Fruit
     protected override IEnumerator Grow()
     {
         yield return base.Grow();
-        StartCoroutine(Fall());
+        yield return StartCoroutine(Shake());
+        yield return StartCoroutine(Fall());
+    }
+
+    private IEnumerator Shake()
+    {
+        float timer = 0;
+
+        while (timer < shakeDuration)
+        {
+            timer += Time.deltaTime;
+
+            float OffsetX = Mathf.Sin(timer * shakeSpeed) * shakeMagnitude;
+            transform.position = _spawnPosition + new Vector3(OffsetX, 0, 0);
+            yield return null;
+        }
+
+        transform.position = _spawnPosition;
     }
 
     private IEnumerator Fall()
