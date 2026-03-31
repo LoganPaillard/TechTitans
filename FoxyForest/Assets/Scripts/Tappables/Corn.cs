e20241d2de4738477f91f227f4b36383d2732c39
LoganPaillard <paillardl@mymacewan.ca>
2026-03-30 20:51:09 -0600

    More falling entities
    Made corn, plum, and acorn into falling entities.

--- FoxyForest/Assets/Scripts/Tappables/Corn.cs
+++ FoxyForest/Assets/Scripts/Tappables/Corn.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class Corn: Entity
+public class Corn: FallingEntity
 {
     public override int score => 20;
     public override float respawnTime => Random.Range(2f, 4f);
